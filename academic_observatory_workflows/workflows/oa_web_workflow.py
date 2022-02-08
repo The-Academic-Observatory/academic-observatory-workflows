1dc48cfb255ad1c1b00707c7c7eec7a4713812a0
Aniek Roelofs <aniek.roelofs@curtin.edu.au>
2022-02-08 14:44:48 +0800

    INF-321: Add wikipedia descriptions

--- academic_observatory_workflows/workflows/oa_web_workflow.py
+++ academic_observatory_workflows/workflows/oa_web_workflow.py
@@ -23,9 +23,10 @@ import math
 import os
 import os.path
 import shutil
+from concurrent.futures import ThreadPoolExecutor, as_completed
 from dataclasses import field
 from operator import itemgetter
-from typing import Optional, List, Dict, Union, Tuple
+from typing import Dict, List, Optional, Tuple, Union
 from urllib.parse import urlparse
 
 import google.cloud.bigquery as bigquery
@@ -34,23 +35,24 @@ import pandas as pd
 import pendulum
 import pyarrow as pa
 import pyarrow.parquet as pq
+import requests
 from airflow.exceptions import AirflowException
 from airflow.models.variable import Variable
 from airflow.sensors.external_task import ExternalTaskSensor
-from pyarrow import json as pa_json
-
-from academic_observatory_workflows.clearbit import clearbit_download_logo
-from observatory.platform.utils.airflow_utils import AirflowVars
+from observatory.platform.utils.airflow_utils import AirflowVars, get_airflow_connection_password
 from observatory.platform.utils.config_utils import module_file_path
 from observatory.platform.utils.file_utils import load_jsonl
 from observatory.platform.utils.gc_utils import (
-    select_table_shard_dates,
     bigquery_sharded_table_id,
     download_blobs_from_cloud_storage,
+    select_table_shard_dates,
 )
 from observatory.platform.utils.workflow_utils import make_release_date
 from observatory.platform.workflows.snapshot_telescope import SnapshotRelease
 from observatory.platform.workflows.workflow import Workflow
+from pyarrow import json as pa_json
+
+from academic_observatory_workflows.clearbit import clearbit_download_logo
 
 # The minimum number of outputs before including an entity in the analysis
 INCLUSION_THRESHOLD = 1000
@@ -403,6 +405,64 @@ class Entity:
         return dict_
 
 
+def get_institution_logo(ror_id: str, url: str, size: str, width: int, fmt: str, build_path) -> Tuple[str, str]:
+    """Get the path to the logo for an institution.
+    If the logo does not exist in the build path yet, download from the Clearbit Logo API tool.
+    If the logo does not exist and failed to download, the path will default to "/unknown.svg".
+
+    :param ror_id: the institution's ROR id
+    :param url: the URL of the company domain + suffix e.g. spotify.com
+    :param size: the image size of the small logo for tables etc.
+    :param width: the width of the image.
+    :param fmt: the image format.
+    :param build_path: the build path for files of this workflow
+    :return: The ROR id and relative (from build path) path to the logo
+    """
+    logo_path = f"/unknown.svg"
+
+    file_path = os.path.join(build_path, "logos", "institution", size, f"{ror_id}.{fmt}")
+    if not os.path.isfile(file_path):
+        clearbit_download_logo(company_url=url, file_path=file_path, size=width, fmt=fmt)
+    if os.path.isfile(file_path):
+        logo_path = make_logo_url(category="institution", entity_id=ror_id, size=size, fmt=fmt)
+
+    return ror_id, logo_path
+
+
+def get_wiki_description(titles: dict) -> List[tuple]:
+    """Get the wikipedia descriptions for the given titles.
+
+    :param titles: Dict with titles as keys and id's (from index table) as values
+    :return: List with (id, wiki description) tuples
+    """
+    # Add &explaintext=1 for plaintext instead of html
+    response = requests.get(
+        f"https://en.wikipedia.org/w/api.php?action=query&format=json&prop=extracts"
+        f"&titles={'%7C'.join(list(titles.keys()))}&redirects=1&exintro=1"
+    )
+    if response.status_code != 200:
+        raise AirflowException()
+    response_json = response.json()
+
+    # Create mapping between normalized title and original title
+    normalized = {}
+    for title in response_json["query"].get("normalized"):
+        normalized[title["to"]] = title["from"]
+
+    # Get description and link to the entity_id from the index table
+    descriptions = []
+    for page in response_json["query"]["pages"].values():
+        description = page["extract"]
+        title = page["title"]
+        # Link original title to description
+        try:
+            entity_id = titles[title]
+        except KeyError:
+            entity_id = titles[normalized[title]]
+        descriptions.append((entity_id, description))
+    return descriptions
+
+
 def bq_query_to_gcs(*, query: str, project_id: str, destination_uri: str, location: str = "us") -> bool:
     """Run a BigQuery query and save the results on Google Cloud Storage.
 
@@ -746,13 +806,10 @@ class OaWebRelease(SnapshotRelease):
                     df.loc[i, key] = value
 
     def update_index_with_logos(self, category: str, df_index_table: pd.DataFrame):
-        """Update the index with logos, downloading logos if the don't exist.
+        """Update the index with logos, downloading logos if they don't exist.
 
         :param category: the category, i.e. country or institution.
         :param df_index_table: the index table Pandas dataframe.
-        :param size: the image size of the small logo for tables etc.
-        :param fmt: the image format.
-        :param size_lg: the image size of the large logo for details pages.
         :return: None.
         """
 
@@ -763,15 +820,16 @@ class OaWebRelease(SnapshotRelease):
 
         # Make logos
         if category == "country":
-            # Copy and rename logo images from using alpha2 to alpha3 country codes
-            for size in sizes:
-                base_path = os.path.join(self.build_path, "logos", category, size)
-                for i, row in df_index_table.iterrows():
-                    alpha3 = row["id"]
-                    alpha2 = row["alpha2"]
-                    src_path = os.path.join(self.data_path, "flags", size, f"{alpha2}.svg")
-                    dst_path = os.path.join(base_path, f"{alpha3}.svg")
-                    shutil.copy(src_path, dst_path)
+            with ThreadPoolExecutor() as executor:
+                futures = []
+                # Copy and rename logo images from using alpha2 to alpha3 country codes
+                for size in sizes:
+                    base_path = os.path.join(self.build_path, "logos", category, size)
+                    for alpha3, alpha2 in zip(df_index_table["id"], df_index_table["alpha2"]):
+                        src_path = os.path.join(self.data_path, "flags", size, f"{alpha2}.svg")
+                        dst_path = os.path.join(base_path, f"{alpha3}.svg")
+                        futures.append(executor.submit(shutil.copy, src_path, dst_path))
+                [f.result() for f in as_completed(futures)]
 
             # Add logo urls to index
             for size in sizes:
@@ -780,24 +838,53 @@ class OaWebRelease(SnapshotRelease):
                 )
         elif category == "institution":
             fmt = "jpg"
-            logo_path_unknown = f"/unknown.svg"
+            # Get the institution logo and the path to the logo image
             for size, width in zip(sizes, [32, 128]):
-                base_path = os.path.join(self.build_path, "logos", category, size)
-                logos = []
-                for i, row in df_index_table.iterrows():
-                    ror_id = row["id"]
-                    url = clean_url(row["url"])
-                    logo_path = logo_path_unknown
-                    if not pd.isnull(url):
-                        file_path = os.path.join(base_path, f"{ror_id}.{fmt}")
-                        if not os.path.isfile(file_path):
-                            clearbit_download_logo(company_url=url, file_path=file_path, size=width, fmt=fmt)
+                with ThreadPoolExecutor() as executor:
+                    futures = []
+                    for ror_id, url in zip(df_index_table["id"], df_index_table["url"]):
+                        url = clean_url(url)
+                        if url:
+                            futures.append(
+                                executor.submit(
+                                    get_institution_logo, ror_id, url, category, size, width, fmt, self.build_path
+                                )
+                            )
+                    logo_paths = [f.result() for f in as_completed(futures)]
 
-                        if os.path.isfile(file_path):
-                            logo_path = make_logo_url(category=category, entity_id=ror_id, size=size, fmt=fmt)
+                # Sort table and results by id
+                df_index_table.sort_index(inplace=True)
+                logo_paths_sorted = [tup[1] for tup in sorted(logo_paths, key=lambda tup: tup[0])]
 
-                    logos.append(logo_path)
-                df_index_table[f"logo_{size}"] = logos
+                # Add logo paths to table
+                df_index_table[f"logo_{size}"] = logo_paths_sorted
+
+    def update_index_with_wiki_descriptions(self, df_index_table: pd.DataFrame):
+        """Get the wikipedia descriptions for each entity (institution or country) and add them to the index table.
+
+        :param df_index_table: the index table Pandas dataframe.
+        :return: None.
+        """
+        # Create list with dictionaries of max 50 ids + titles (this is wiki api max)
+        # The wikipedia 'title' is the last part of the wikipedia url
+        titles_all = list(zip(df_index_table["wikipedia_url"].str.split("/").str[-1], df_index_table["id"]))
+        titles_chunks = [dict(titles_all[i : i + 50]) for i in range(0, len(titles_all), 50)]
+
+        # Process each dictionary in separate thread to get wiki descriptions
+        with ThreadPoolExecutor() as executor:
+            futures = []
+            for titles in titles_chunks:
+                futures.append(executor.submit(get_wiki_description, titles))
+            descriptions = []
+            for f in as_completed(futures):
+                descriptions += f.result()
+
+        # Sort table and results by id
+        df_index_table.sort_index(inplace=True)
+        descriptions_sorted = [tup[1] for tup in sorted(descriptions, key=lambda tup: tup[0])]
+
+        # Add wiki descriptions to table
+        df_index_table["description"] = descriptions_sorted
 
     def save_index(self, category: str, df_index_table: pd.DataFrame):
         """Save the index table.
@@ -1179,6 +1266,7 @@ class OaWebWorkflow(Workflow):
             # Make index table
             df_index_table = release.make_index(category, df)
             release.update_index_with_logos(category, df_index_table)
+            release.update_index_with_wiki_descriptions(df_index_table)
             entities = release.make_entities(df_index_table, df)
 
             # Make autocomplete data for this category
