dfc0226cc8223dbc4035eb7ad0a66f0485711b13
Aniek Roelofs <aniek.roelofs@curtin.edu.au>
2022-02-14 13:39:59 +0800

    Set CLOUDSK_PYTHON env to fix gcloud error

--- requirements.sh
+++ requirements.sh
@@ -22,7 +22,10 @@
 # Crossref Metadata (pigz), Open Citations (unzip)
 apt-get install pigz unzip -y
 
-# ORCID: install Google Cloud SDK as root user, used in
+# ORCID: install Google Cloud SDK as root user
 echo "deb [signed-by=/usr/share/keyrings/cloud.google.gpg] http://packages.cloud.google.com/apt cloud-sdk main" | \
 tee -a /etc/apt/sources.list.d/google-cloud-sdk.list && curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | \
 apt-key --keyring /usr/share/keyrings/cloud.google.gpg  add - && apt-get update -y && apt-get install google-cloud-sdk -y
+
+# ORCID: fix gcloud error, see https://issuetracker.google.com/issues/217589135
+echo "export CLOUDSDK_PYTHON=python3" >> /home/airflow/.profile
