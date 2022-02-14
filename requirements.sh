9eebe90a562a975a68357f633b9c9ba57c784d2b
Aniek Roelofs <aniek.roelofs@curtin.edu.au>
2022-02-14 14:03:07 +0800

    Set CLOUDSK_PYTHON env to fix gcloud error inside telescope

--- requirements.sh
+++ requirements.sh
@@ -26,6 +26,3 @@ apt-get install pigz unzip -y
 echo "deb [signed-by=/usr/share/keyrings/cloud.google.gpg] http://packages.cloud.google.com/apt cloud-sdk main" | \
 tee -a /etc/apt/sources.list.d/google-cloud-sdk.list && curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | \
 apt-key --keyring /usr/share/keyrings/cloud.google.gpg  add - && apt-get update -y && apt-get install google-cloud-sdk -y
-
-# ORCID: fix gcloud error, see https://issuetracker.google.com/issues/217589135
-echo "export CLOUDSDK_PYTHON=python3" >> /home/airflow/.profile
