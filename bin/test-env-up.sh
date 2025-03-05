d0352fb7c9a281b9d6fc67b0d8b78f6c1801fa4f
keeagnsmith21 <keegan.r.s21@gmail.com>
2025-03-05 16:07:24 +0800

    test

--- bin/test-env-up.sh
+++ bin/test-env-up.sh
@@ -50,6 +50,7 @@ kubectl apply -f bin/test-konfig.yaml
 
 # Unset the minikube docker environment
 eval $(minikube docker-env --unset)
+echo $(minikube ssh grep host.minikube.internal /etc/hosts | cut -f1) host.minikube.internal >> /etc/hosts
 
 echo ""
 echo "########################### Minikube cluster running ###########################"
