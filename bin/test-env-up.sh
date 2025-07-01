5027be4fdc1e62630e4a60fbf09d4943e9f0f717
keeagnsmith21 <keegan.r.s21@gmail.com>
2025-07-01 15:07:44 +0800

    Try official minikube action image

--- bin/test-env-up.sh
+++ bin/test-env-up.sh
@@ -4,20 +4,53 @@ set -euxo pipefail
 usage() {
     echo "This script builds and starts the Academic Observatory test environment"
     echo "Usage: $0 [--no-build]"
+    echo "       $0 [--remote]"
     echo "       $0 [--help]"
     echo
     echo "--no-build  Will not build the AO workflows Docker image."
+    echo "--remote    Will run as if in Github Actions"
     echo "--help      Display this help message"
     exit 1
 }
 
-arg="${1:-}"
-if [ "$arg" == "--help" ]; then
-    usage
-elif [ "$arg" != "--no-build" ] && [ -n "$arg" ]; then
-    echo "Invalid argument: $arg"
-    usage
-fi
+
+get_args() {
+    local OPTIONS
+    OPTIONS=$(getopt -o h --long help,no-build,remote -- "$@")
+    if [ $? -ne 0 ]; then
+        usage
+        exit 1
+    fi
+
+    eval set -- "$OPTIONS"
+    while true; do
+        case "$1" in
+            -h|--help)
+                usage
+                ;;
+            --no-build)
+                nobuild=true
+                shift
+                ;;
+            --remote)
+                remote=true
+                shift
+                ;;
+            --)
+                shift
+                break
+                ;;
+            *)
+                echo "Unexpected option: $1"
+                usage
+                ;;
+        esac
+    done
+}
+
+nobuild=false
+remote=false
+get_args "$@"
 
 # Authenticate minikube with gcp
 if [ -f .env ]; then # Source the .env file
@@ -30,42 +63,21 @@ fi
 export GOOGLE_APPLICATION_CREDENTIALS=$GOOGLE_APPLICATION_CREDENTIALS
 
 # Kill anything that's using our ports
-# sudo fuser -k 5080/tcp || true
-# sudo fuser -k 5021/tcp || true
+sudo fuser -k 5080/tcp || true
+sudo fuser -k 5021/tcp || true
 
 # Delete and start Minikube
-minikube delete --all --purge
-minikube start \
-    --ports=5080,5021 \
-    --extra-config=apiserver.service-node-port-range=30000-30009 \
-    --wait=all \
-    --wait-timeout=2m0s \
-    --force \
-    --driver=docker
-minikube update-context
-
-echo "=== DEBUG: minikube status ==="
-minikube status
-
-echo "=== DEBUG: Minikube kubeconfig contents ==="
-cat "$HOME/.kube/config"
-
-# Ensure correct context is used
-export KUBECONFIG="$HOME/.kube/config"
-kubectl config use-context minikube
-
-echo "=== DEBUG: kubectl current context ==="
-kubectl config current-context || echo "No current context set"
-
-echo "=== DEBUG: kubectl config view (minified) ==="
-kubectl config view --minify || echo "No config found"
-
-echo "=== DEBUG: kubeconfig file exists? ==="
-ls -l "$HOME/.kube/config" || echo "Missing ~/.kube/config"
-
-echo "=== DEBUG: trying to reach cluster ==="
-kubectl cluster-info || echo "cluster-info failed"
-
+if [ -n "${remote}" ]; then
+    minikube delete --all --purge
+    minikube start \
+        --ports=5080,5021 \
+        --extra-config=apiserver.service-node-port-range=30000-30009 \
+        --wait=all \
+        --wait-timeout=2m0s \
+        --force \
+        --driver=docker
+    minikube update-context
+fi
 
 # Wait until the API is ready
 for _ in {1..30}; do
@@ -76,8 +88,14 @@ for _ in {1..30}; do
     echo "Waiting for Kubernetes API..." && sleep 2
 done
 
-# Enable addons
-minikube addons enable gcp-auth
+# Ensure correct context is used
+if [ -n "${remote}" ]; then
+    export KUBECONFIG="$HOME/.kube/config"
+    kubectl config use-context minikube
+
+    # Enable addons
+    minikube addons enable gcp-auth
+fi
 
 # Run docker-compose services
 docker compose -f test-env-compose.yaml build
@@ -86,7 +104,7 @@ docker compose -f test-env-compose.yaml up -d
 
 # Use the minikube Docker daemon
 eval "$(minikube docker-env)"
-if [ "$1" != "--no-build" ]; then
+if [ -n "${nobuild}" ]; then
     docker build --no-cache -t academic-observatory:test .
 fi
 
@@ -104,4 +122,5 @@ echo "bash bin/test-env-down.sh"
 echo ""
 echo "--Monitor the cluster--"
 echo "minikube dashboard"
-echo "################################################################################"
+echo ""
+rcho "################################################################################"
