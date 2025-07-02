c11c188ac7ef64ce8d862fcc4fdb61685a670d3c
Keegan Smith <keegan.r.s21@gmail.com>
2025-07-02 11:31:50 +0800

    Minikube actions fix (#245)

--- bin/test-env-up.sh
+++ bin/test-env-up.sh
@@ -1,22 +1,56 @@
 #!/usr/bin/env bash
-set -e
+set -euxo pipefail
 
 usage() {
     echo "This script builds and starts the Academic Observatory test environment"
     echo "Usage: $0 [--no-build]"
+    echo "       $0 [--remote]"
     echo "       $0 [--help]"
     echo
     echo "--no-build  Will not build the AO workflows Docker image."
+    echo "--remote    Will run as if in Github Actions"
     echo "--help      Display this help message"
     exit 1
 }
-if [ "$1" == "--help" ]; then
-    usage
-elif [ "$1" != "--no-build" ] && [ "$1" != "--help" ] && [ ! -z "$1" ]; then
-    echo "Invalid argument: $1"
-    usage
-fi
 
+
+get_args() {
+    local OPTIONS
+    OPTIONS=$(getopt -o h --long help,no-build,remote -- "$@")
+    if [ $? -ne 0 ]; then
+        usage
+        exit 1
+    fi
+
+    eval set -- "$OPTIONS"
+    while true; do
+        case "$1" in
+            -h|--help)
+                usage
+                ;;
+            --no-build)
+                nobuild=true
+                shift
+                ;;
+            --remote)
+                remote=true
+                shift
+                ;;
+            --)
+                shift
+                break
+                ;;
+            *)
+                echo "Unexpected option: $1"
+                usage
+                ;;
+        esac
+    done
+}
+
+nobuild=false
+remote=false
+get_args "$@"
 
 # Authenticate minikube with gcp
 if [ -f .env ]; then # Source the .env file
@@ -26,28 +60,62 @@ if [ -z "${GOOGLE_APPLICATION_CREDENTIALS}" ]; then
     echo "GOOGLE_APPLICATION_CREDENTIALS is not set in '.env'. This is required to run the tests."
     exit 1
 fi
-export GOOGLE_APPLICATION_CREDENTIALS=$GOOGLE_APPLICATION_CREDENTIALS
+export USE_NATIVE_LOOP="false" # asyncio issue fix
 
-# Check if minikube is running, if not, start it
-minikube delete --all --purge
-minikube start --ports=5080,5021 --extra-config=apiserver.service-node-port-range=30000-30009 --network=bridge --wait-timeout=2m0s --force
-minikube addons enable gcp-auth
+# Kill anything that's using our ports
+sudo fuser -k 5080/tcp || true
+sudo fuser -k 5021/tcp || true
 
+# Delete and start Minikube
+if [ "${remote}" = "false" ]; then
+    minikube delete --all --purge
+    minikube start \
+        --ports=5080,5021 \
+        --extra-config=apiserver.service-node-port-range=30000-30009 \
+        --wait=all \
+        --wait-timeout=2m0s \
+        --force \
+        --driver=docker
+    minikube update-context
+fi
 
-# Run the compose commands to spin up the servers
+# Wait until the API is ready
+for _ in {1..30}; do
+    if kubectl get nodes &>/dev/null; then
+        echo "Kubernetes API is ready."
+        break
+    fi
+    echo "Waiting for Kubernetes API..." && sleep 2
+done
+
+# Ensure correct context is used
+export KUBECONFIG="$HOME/.kube/config"
+# TODO: delete these if tests pass
+# export AIRFLOW__KUBERNETES__DELETE_WORKER_PODS=True
+# export AIRFLOW__KUBERNETES__WORKER_PODS_CREATION_BATCH_SIZE=1
+# export AIRFLOW__KUBERNETES__POD_MANAGER_ENABLE_ASYNC=False
+kubectl config use-context minikube
+
+if [ "${remote}" = "false" ]; then
+    # Enable addons
+    minikube addons enable gcp-auth
+fi
+
+# Run docker-compose services
 docker compose -f test-env-compose.yaml build
 docker compose -f test-env-compose.yaml down
 docker compose -f test-env-compose.yaml up -d
 
-# Use the minikube docker daemon to build Academic Observatory workflows image
-eval $(minikube docker-env)
-if [ "$1" != "--no-build" ]; then
+# Use the minikube Docker daemon
+eval "$(minikube docker-env)"
+if [ "${nobuild}" = "false" ]; then
     docker build --no-cache -t academic-observatory:test .
 fi
 
 # (Re)Deploy kubernetes config items
 kubectl delete --ignore-not-found -f bin/test-konfig.yaml
 kubectl apply -f bin/test-konfig.yaml
+kubectl cluster-info
 
 echo ""
 echo "########################### Minikube cluster running ###########################"
@@ -58,4 +126,5 @@ echo "bash bin/test-env-down.sh"
 echo ""
 echo "--Monitor the cluster--"
 echo "minikube dashboard"
+echo ""
 echo "################################################################################"
